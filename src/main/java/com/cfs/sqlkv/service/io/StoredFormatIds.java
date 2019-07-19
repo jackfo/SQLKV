589a59292fe7a5e66d101f82bdc03b91ad438668
jackfadp <jackfadp@gmail.com>
2019-07-19 09:18:43 +0800

    数据库系统设计

--- src/main/java/com/cfs/sqlkv/service/io/StoredFormatIds.java
+++ src/main/java/com/cfs/sqlkv/service/io/StoredFormatIds.java
@@ -1,25 +1,3 @@
-/*
-
-   Derby - Class com.cfs.sqlkv.service.io.StoredFormatIds
-
-   Licensed to the Apache Software Foundation (ASF) under one or more
-   contributor license agreements.  See the NOTICE file distributed with
-   this work for additional information regarding copyright ownership.
-   The ASF licenses this file to you under the Apache License, Version 2.0
-   (the "License"); you may not use this file except in compliance with
-   the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
- */
-
-//depot/main/java/com.cfs.sqlkv.service.io/StoredFormatIds.java#211 - edit change 20974 (text)
 package com.cfs.sqlkv.service.io;
 /**
   A format id identifies a stored form of an object for the
